7d4fd930548f0a13dbcd69c4e666238b54e8b357
Bailey Townsend <baileytownsend2323@gmail.com>
2024-08-12 01:28:42 +0000

    We are where we are

--- src/views/LandingView.vue
+++ src/views/LandingView.vue
@@ -17,7 +17,7 @@ import ArticleList from "../components/ArticleList.vue";
 						Hey, i'm Bailey or FatFingers 👋			
 				</h1>
 				<p class="py-6 font-poppins lg:max-w-lg">
-						 Sometimes serious, sometimes not. In my day job, I'm a team lead and full stack developer for a dotnet web application. In my free time, I follow whatever interests me. Be it retro gaming, retro console modding,  microcontrollers, or anything else. Usually, it changes from week to week. I plan on covering my current hyperfixation here.
+						 Sometimes serious, sometimes not. In my day job, I'm a team lead and full stack developer for a dotnet web application. In my free time, I follow whatever interests me. Be it retro gaming, retro console modding, microcontrollers, or anything else. Usually, it changes from week to week. I plan on covering my current hyperfixation here.
 				</p>
 				<div class="flex gap-2 max-lg:justify-center">
 						<a href="https://github.com/fatfingers23" class="btn btn-neutral md:btn-lg rounded-full font-poppins shadow-md">

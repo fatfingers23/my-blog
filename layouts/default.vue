2f4fb0c384174d1124d8f7bf80afc96f778ea210
Bailey Townsend <baileytownsend2323@gmail.com>
2024-08-12 22:27:47 -0500

    Added the speed test thing

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,13 @@
 <script setup lang="ts">
+import { SpeedInsights } from "@vercel/speed-insights/nuxt";
 
 </script>
 
 <template>
   <main class="bg-base-200">
     <div class="container mx-auto md:p-8 p-3 min-h-screen">
-      <!--      <RouterView />-->
       <slot />
+      <SpeedInsights />
     </div>
   </main>
 </template>
